f1cef4da10d3a5fadee99bc586e2d1ee019bb9c5
User1 <User1@User>
2025-04-29 19:10:19 +0530

    new deleted and updated

---
+++ practise project/functions/exercise_program.py
@@ -0,0 +1,53 @@
+'''
+Created on 29-Apr-2025
+
+@author: User1
+'''
+'''
+#if a number is even or odd
+
+num = int(input("Please enter a number: "))
+
+if num % 2 == 0:
+    print(f"The number {num} is Even.")
+else:
+    print(f"The number {num} is Odd.")
+
+print(":)")
+
+'''
+'''
+#greater than or not 
+a = 5
+b = 8
+
+if a > b:
+    print(f"{a} is greater than {b}")
+else:
+    print(f"{b} is greater than {a}")
+'''    
+    
+'''
+#voter age 
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to vote!")
+else:
+    print("Sorry, you are not eligible to vote yet.")
+'''
+
+#largest of three
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+if a >= b and a >= c:
+    largest = a
+elif b >= a and b >= c:
+    largest = b
+else:
+    largest = c
+
+print("The largest number is:", largest)
---
+++ practise project/functions/functions_.py
@@ -0,0 +1,47 @@
+'''
+Created on 27-Apr-2025
+
+@author: Yashwanth
+'''
+'''
+function is a  block of reuseable code to perform a specific task 
+
+types of code 
+1. predefined > readily available in python
+2. userdefined>  created by programer 
+
+why we need function
+1. reduce repeatition of code/ reduce cpmplexity, minimize the no. of lines 
+of code , this help in increased code readability 
+
+sayntax
+
+def function_name(parameteres):
+<code>
+
+def -> keywoord to define function 
+function_name -> lower case separated by _ (manatory)
+(): -> mandatory 
+
+
+'''
+'''
+a = 2
+b = 5
+c= a+b
+print(f"Sum of {a} and {b}:",c)
+
+a = 6
+b = 5
+c= a+b
+print(f"Sum of {a} and {b}:",c)
+'''
+
+def welcome():
+    print("hello world! hai")
+    print("i hope yor're having a great day")
+    
+welcome()
+welcome()
+
+
