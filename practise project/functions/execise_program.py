e14b474feaf0b7fd15661d8225119444fadca4dd
User1 <User1@User>
2025-04-29 09:27:00 +0530

    new update

---
+++ practise project/functions/execise_program.py
@@ -0,0 +1,38 @@
+'''
+Created on 29-Apr-2025
+
+@author: User1
+'''
+'''
+#if a number is even or odd
+
+num = int(input("Please enter a number: "))
+
+if num % 2 == 0:
+    print(f"The number {num} is Even.")
+else:
+    print(f"The number {num} is Odd.")
+
+print(":)")
+
+'''
+'''
+#greater than or not 
+a = 5
+b = 8
+
+if a > b:
+    print(f"{a} is greater than {b}")
+else:
+    print(f"{b} is greater than {a}")
+'''    
+    
+'''
+#voter age 
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to vote!")
+else:
+    print("Sorry, you are not eligible to vote yet.")
+'''
