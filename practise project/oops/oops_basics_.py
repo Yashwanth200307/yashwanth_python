05a802ba0da9904bac9622db73c534185666ada4
User1 <User1@User>
2025-05-04 14:16:18 +0530

    oops 04-05

---
+++ practise project/oops/oops_basics_.py
@@ -0,0 +1,21 @@
+'''
+Created on 04-May-2025
+
+@author: Yashwanth 
+'''
+'''
+Methods are functions which are defined inside a class we can call methods using the objects of that class  
+
+* method of one class can be using an object of that class..
+'''
+class Humanbeings():
+    def walk(self):
+        print("iam walking")
+        print("i am eating")
+        
+obj1=Humanbeings()
+obj1.walk()
+
+#print(type(obj1)) #<class '__main__.Humanbeings'>
+#types give wich type if variable that belongs to 
+
