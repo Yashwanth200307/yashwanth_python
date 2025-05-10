96820bb9b9eedcc1242231e13bf80a8ddce9e0b4
User1 <User1@User>
2025-05-10 11:12:33 +0530

    new update 10-05

--- practise project/oops/oops_basics_.py
+++ practise project/oops/oops_basics_.py
@@ -7,6 +7,9 @@ Created on 04-May-2025
 Methods are functions which are defined inside a class we can call methods using the objects of that class  
 
 * method of one class can be using an object of that class..
+what is constructor(__init__) it is a magic method used to construct an object by 
+initialize specific values being while creating the object 
+'''
 '''
 class Humanbeings():
     def walk(self):
@@ -15,16 +18,17 @@ class Humanbeings():
         
 obj1=Humanbeings()
 obj1.walk()
-
+'''
 #print(type(obj1)) #<class '__main__.Humanbeings'>
 #types give wich type if variable that belongs to 
 
 class Humanbeings():
     def __init__(self, name):
-        pass
+        self.name=name
+    
     def walk(self):
-        print("iam walking")
-        print("i am eating")
+        print(f"{self.name} need to walk")
+        print(f"{self.name} need to eat")
         
-obj1=Humanbeings(Yashu)
+obj1=Humanbeings("Yashwanth")
 obj1.walk()
