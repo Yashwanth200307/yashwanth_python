7e1c09ee3653b84ed70d5f377ab056788f032d78
User1 <User1@User>
2025-05-14 21:32:14 +0530

    new selenium folder and updats 14-05

---
+++ _Selenium_Tutorials_/basics_/first_program.py
@@ -0,0 +1,40 @@
+'''
+Created on 11-May-2025
+
+@author: User1
+'''
+'''
+from selenium import webdriver
+import time
+
+options = webdriver.ChromeOptions()
+options.add_argument("start-maximized") 
+
+driver = webdriver.Chrome(options=options) 
+driver.get('https://selenium.dev/')
+
+time.sleep(30)
+'''
+
+'''
+#Launching the chrome browser
+from selenium import webdriver
+
+
+driver = webdriver.Chrome()
+driver.get('https://selenium.dev/')
+'''
+#optoions = webdriver.ChromeOptions() 
+
+from selenium import webdriver
+
+
+
+
+options = webdriver.ChromeOptions()
+options.add_argument("start-maximized") #used for maxing the screen 
+
+options.add_experimental_option("detach", True) #used to hold the screen
+driver = webdriver.Chrome(options)
+
+driver.get('https://selenium.dev/') #the chrome selenium
